516c1e4881f1eb3982b112bbb67396c54f548798
unknown <builecongduy@gmail.com>
2024-12-01 20:52:45 +0700

    projectduy

---
+++ TestCase3.py
@@ -0,0 +1,43 @@
+import pytest
+import time
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+class TestLogin:
+    def setup_method(self, method):
+        options = Options()
+        options.add_experimental_option("detach", True)
+        self.driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=options)
+        self.driver.maximize_window()
+        self.driver.get("https://viblo.asia/") 
+
+    def teardown_method(self, method):
+        self.driver.quit()
+
+    def test_login(self):
+        time.sleep(3)
+        click_btn_logIn = self.driver.find_element(By.CLASS_NAME, "login-button")
+        click_btn_logIn.click()
+        def click_login():
+            login_button = self.driver.find_element(By.XPATH, "//button[@class='el-button w-100 el-button--primary']")
+            login_button.click()
+        try:
+            info = self.driver.find_elements(By.CLASS_NAME, "el-input__inner")
+            username = info[0]
+            password = info[1]
+            username.clear()
+            password.clear()
+
+            username.send_keys("")
+            password.send_keys("")
+            click_login()
+            time.sleep(5)
+            if "Wrong username/email or password" in self.driver.page_source:
+                print("Test passed: Incorrect credentials message displayed")
+            else:
+                print("Test failed: No error message displayed")
+        except Exception as e:
+            print(e)
